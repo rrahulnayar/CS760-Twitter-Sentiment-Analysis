9a1420a6bd10546f8793d400d343951ab5c16921
cgarg92 <chirayu.garg92@gmail.com>
2018-04-30 23:42:54 -0500

    Add rnn, blstm support, cmdline args options

--- lstmTrain.py
+++ lstmTrain.py
@@ -8,12 +8,15 @@ import pylab
 import pickle
 import tensorflow as tf
 import numpy as np
+import argparse
 
-
-def lstm_cell():
-	global lstm_size
+def lstm_cell(lstm_size,keep_prob):
 	lstm = tf.contrib.rnn.BasicLSTMCell(lstm_size, reuse=tf.get_variable_scope().reuse)
 	return tf.contrib.rnn.DropoutWrapper(lstm, output_keep_prob=keep_prob)
+
+def rnn_cell(lstm_size,keep_prob):
+	rnn = tf.contrib.rnn.BasicRNNCell(lstm_size, reuse=tf.get_variable_scope().reuse)
+	return tf.contrib.rnn.DropoutWrapper(rnn, output_keep_prob=keep_prob)
 
 def get_batches(x, y, batchSize=100):
 	nBatches = len(x) #batchSize
@@ -54,6 +57,27 @@ with open('./data/train.pkl', 'rb') as train:
 	# Max Size of Input Review : 228
 	# First index for the max size : 11357
 
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-units', type=int, dest='numUnits', default=256)
+	parser.add_argument('-layers', type=int, dest='numLayers', default=2)
+	parser.add_argument('-batch', type=int, dest='batchSize', default=200)
+	parser.add_argument('-lr', type=float, dest='lr', default=0.02)
+	parser.add_argument('-ntype', type=str, dest='ntype', default='lstm')
+	parser.add_argument('-optimizer', type=str, dest='opt', default='adam')
+	args = parser.parse_args()
+
+	useSGD = (args.opt == 'sgd')
+	memoryNDtype = args.ntype
+	lstm_size = args.numUnits
+	lstm_layers = args.numLayers
+	batchSize = args.batchSize
+	learningRate = args.lr
+
+	config = memoryNDtype + '_' + str(lstm_size) + 'u_' + str(lstm_layers) + 'l_' + str(batchSize) + 'b_' + args.opt + '_' + str(learningRate) + 'lr'
+	print (config)
+	if not os.path.exists(config):
+		os.makedirs(config)
+
 	seq_len = 100
 	features = np.zeros((len(dataTrain[0]), seq_len), dtype=int)
 	labelsRaw = np.expand_dims(np.array(dataTrain[1]), axis=1)
@@ -87,14 +111,12 @@ with open('./data/train.pkl', 'rb') as train:
 
 	#print(len(nWords))
 	#LSTM paramerters
-	lstm_size = 256
-	lstm_layers = 2
-	batchSize = 500
-	learningRate = 0.01
+
 	dictFile = open('./data/dictionary.pkl', 'rb')
 	nWords = pickle.load(dictFile)
-	n_words = len(nWords) + 1 # add 1 for 0 added to the vocabulary
+	n_words = len(nWords) + 2 # add 1 for 0 added to the vocabulary
 
+	os.chdir(config)
 	# Create a graph object
 	tf.reset_default_graph()
 	with tf.name_scope('inputs'):
@@ -108,12 +130,28 @@ with open('./data/train.pkl', 'rb') as train:
 		embed = tf.nn.embedding_lookup(embedding, inputs_)
 
 	with tf.name_scope("RNN_layers"):
-		cell = tf.contrib.rnn.MultiRNNCell([lstm_cell() for _ in range(lstm_layers)])
-		initial_state = cell.zero_state(batchSize, tf.float32)
+		if memoryNDtype == 'lstm':
+			cell = tf.contrib.rnn.MultiRNNCell([lstm_cell(lstm_size, keep_prob) for _ in range(lstm_layers)])
+			initial_state = cell.zero_state(batchSize, tf.float32)
+
+		elif memoryNDtype == 'rnn':
+			cell = tf.contrib.rnn.MultiRNNCell([rnn_cell(lstm_size, keep_prob) for _ in range(lstm_layers)])
+			initial_state = cell.zero_state(batchSize, tf.float32)
+
+		elif memoryNDtype == 'blstm':
+			fwdCell = lstm_cell(lstm_size, keep_prob)
+			initialStateFw = fwdCell.zero_state(batchSize, tf.float32)
+			bwdCell = lstm_cell(lstm_size, keep_prob)
+			initialStateBw = bwdCell.zero_state(batchSize, tf.float32)
+			#initial_state = tf.concat([initialStateFw, initialStateBw],2)
 
 
 	with tf.name_scope("RNN_forward"):
-		outputs, final_state = tf.nn.dynamic_rnn(cell, embed, initial_state=initial_state)
+		if memoryNDtype == 'blstm':
+			outputBi, final_state = tf.nn.bidirectional_dynamic_rnn(fwdCell, bwdCell, embed, initial_state_fw=initialStateFw, initial_state_bw=initialStateBw)
+			outputs = tf.concat(outputBi, 2)
+		else:
+			outputs, final_state = tf.nn.dynamic_rnn(cell, embed, initial_state=initial_state)
 
 
 	with tf.name_scope('predictions'):
@@ -125,7 +163,10 @@ with open('./data/train.pkl', 'rb') as train:
 		tf.summary.scalar('cost', cost)
 
 	with tf.name_scope('train'):
-		optimizer = tf.train.AdamOptimizer(learningRate).minimize(cost)
+		if useSGD:
+			optimizer = tf.train.MomentumOptimizer(learningRate, momentum=0.9,use_nesterov=True).minimize(cost)
+		else:
+			optimizer = tf.train.AdamOptimizer(learningRate).minimize(cost)
 
 	merged = tf.summary.merge_all()
 
@@ -143,15 +184,28 @@ with open('./data/train.pkl', 'rb') as train:
 		test_writer = tf.summary.FileWriter('./log/tb/test', sess.graph)
 		iteration = 1
 		for e in range(epochs):
-			state = sess.run(initial_state)
+			if memoryNDtype == 'blstm':
+				stateFw = sess.run(initialStateFw)
+				stateBw = sess.run(initialStateBw)
+			else:
+				state = sess.run(initial_state)
 
 			for i, (x,y) in enumerate(get_batches(train_x, train_y, batchSize), 1):
-				feed = {inputs_: x,
-                        labels_: y[:],
-						keep_prob: 0.5,
-						initial_state: state}
 
-				summary, loss, state, _ = sess.run([merged, cost, final_state, optimizer], feed_dict=feed)
+				if memoryNDtype == 'blstm':
+					feed = {inputs_: x,
+	                        labels_: y[:],
+							keep_prob: 1,
+							initialStateFw: stateFw,
+							initialStateBw: stateBw}
+					summary, loss, stateFw, stateBw, _ = sess.run([merged, cost, final_state[0], final_state[1], optimizer], feed_dict=feed)
+				else:
+					feed = {inputs_: x,
+	                        labels_: y[:],
+							keep_prob: 1,
+							initial_state: state}
+
+					summary, loss, state, _ = sess.run([merged, cost, final_state, optimizer], feed_dict=feed)
 
 				train_writer.add_summary(summary, iteration)
 
@@ -161,15 +215,28 @@ with open('./data/train.pkl', 'rb') as train:
 						  "Train loss: {:.3f}".format(loss))
 				iteration +=1
 			val_acc = []
-			val_state = sess.run(cell.zero_state(batchSize, tf.float32))
+
+			if memoryNDtype == 'blstm':
+				stateFwVal = sess.run(initialStateFw)
+				stateBwVal = sess.run(initialStateBw)
+			else:
+				val_state = sess.run(cell.zero_state(batchSize, tf.float32))
 			# checkpoint
 			for x, y in get_batches(val_x, val_y, batchSize):
-				feed = {inputs_: x,
-						labels_: y[:],
-						keep_prob: 1,
-						initial_state: val_state}
+				if memoryNDtype == 'blstm':
+					feed = {inputs_: x,
+							labels_: y[:],
+							keep_prob: 1,
+							initialStateFw: stateFwVal,
+							initialStateBw: stateBwVal}
+					summary, batch_acc, stateFwVal,stateBwVal  = sess.run([merged, accuracy, final_state[0], final_state[1]], feed_dict=feed)
+				else:
+					feed = {inputs_: x,
+							labels_: y[:],
+							keep_prob: 1,
+							initial_state: val_state}
 #                     batch_acc, val_state = sess.run([accuracy, final_state], feed_dict=feed)
-				summary, batch_acc, val_state = sess.run([merged, accuracy, final_state], feed_dict=feed)
+					summary, batch_acc, val_state = sess.run([merged, accuracy, final_state], feed_dict=feed)
 				val_acc.append(batch_acc)
 			print("Val acc: {:.3f}".format(np.mean(val_acc)))
 
